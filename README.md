b2c6ae8365846d3dceebfd87103fab40a8849d33
John Waweru <wambugu.john.waweru@outlook.com>
2018-09-12 18:26:01 +0300

    finished test

---
+++ README.md
@@ -0,0 +1,25 @@
+# 10TEN TECHNICAL TEST
+
+> Prints out transalations of words based on a JSON data source
+
+## Installation
+
+To install the required dependecies you need to have:
+
+1. node version >=8.11.0
+2. npm version >=5.3.0
+3. you require to have typescript installed globally. Use the command `npm i -g typescript`
+
+### Instructions
+
+1. git clone the project
+2. install dependencies using `npm install`
+3. compile the typescript code `tsc`
+
+## How to run
+
+> npm main.js [{ transalation }]
+
+## Author
+
+John Waweru, waweruj00@gmail.com
---
+++ main.ts
@@ -0,0 +1,20 @@
+/**
+ * created on 12 Sep 2018
+ */
+
+import * as fs from 'fs';
+const countries: Array<{ [key: string]: any }> = require('./source/countries.json');
+
+// get translation from CLI arguments
+const translation: string = process.argv.splice(2)[0];
+
+const filteredCountries = countries.filter(country => (country.translations.hasOwnProperty(translation)));
+
+if (filteredCountries.length === 0) {
+    const err = new Error();
+    err.name = 'TransalationNotFound';
+    err.message = 'Sorry could not find transalation you were looking for.';
+    throw err;
+}
+
+console.log(filteredCountries.map(country => (country.translations[translation].official)).join('\n'))
